56bab428ae5ff466d4c4cec06f232047b6219d55
sryang <sarang628@gmail.com>
2025-08-18 07:36:16 +0800

    update repository

--- RestaurantMenusDataConverter.kt
+++ RestaurantMenusDataConverter.kt
@@ -3,13 +3,14 @@ package com.sarang.torang.di.restaurant_menu_di
 import com.sarang.torang.BuildConfig
 import com.sarang.torang.compose.menu.MenuData
 import com.sarang.torang.data.RestaurantDetail
+import com.sarang.torang.data.remote.response.RestaurantDetailApiModel
 
 
-fun RestaurantDetail.toMenus(): List<MenuData> {
+fun RestaurantDetailApiModel.toMenus(): List<MenuData> {
     return this.menus.map {
         MenuData(
-            menuName = it.menu_name,
-            price = it.menu_price.toFloat(),
+            menuName = it.menu_name ?: "",
+            price = it.menu_price?.toFloat() ?: 0f,
             url = BuildConfig.MENU_IMAGE_SERVER_URL + it.menu_pic_url
         )
     }
