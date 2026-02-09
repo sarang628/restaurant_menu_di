eb4b5cd713c9bb0dc74833c94555567f8b9cd4aa
sryang <sarang628@gmail.com>
2026-02-09 23:36:12 +0900

    add video review

--- CustomRestaurantMenuImageLoader.kt
+++ CustomRestaurantMenuImageLoader.kt
@@ -1,6 +1,20 @@
 package com.sarang.torang.di.restaurant_menu_di
 
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import com.sarang.torang.compose.menu.RestaurantMenuImageLoader
+import com.sarang.torang.di.image.TorangAsyncImageData
 import com.sarang.torang.di.image.provideTorangAsyncImage
 
-val customRestaurantMenuImageLoader : RestaurantMenuImageLoader = provideTorangAsyncImage()
+val customRestaurantMenuImageLoader : RestaurantMenuImageLoader = {
+    modifier, url, progressSize, errorIconSize, contentScale ->
+    provideTorangAsyncImage().invoke(
+        TorangAsyncImageData(
+            modifier = modifier,
+            model = url,
+            progressSize = progressSize ?: 30.dp,
+            errorIconSize = errorIconSize ?: 30.dp,
+            contentScale = contentScale ?: ContentScale.None
+        )
+    )
+}
